674d23e6e1af9b4e10cd6738250d661f28d12fdd
mkuhn <michel.kuhn@adition.com>
2019-02-04 09:33:37 +0100

    move the player

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import pygame
 from pygame.locals import *
 from player import Player
-pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+
+SCRREN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+pygame.init()
+screen = pygame.display.set_mode((SCRREN_WIDTH, SCREEN_HEIGHT))
+player = Player(SCRREN_WIDTH, SCREEN_HEIGHT)
+
+game_objects = pygame.sprite.Group()
+game_objects.add(player)
+
+background = pygame.Surface(screen.get_size())
+background.fill((0, 0, 0))
 
 running = True
-player = Player()
 
 while running:
     for event in pygame.event.get():
@@ -16,5 +27,12 @@ while running:
         elif event.type == QUIT:
             running = False
 
-    screen.blit(player.surf, player.rect)
+    screen.blit(background, (0, 0))
+
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+
+    for entity in game_objects:
+        screen.blit(entity.surf, entity.rect)
+
     pygame.display.flip()
--- player.py
+++ player.py
@@ -1,9 +1,31 @@
 import pygame
+from pygame.locals import *
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, screen_width, screen_height):
         super(Player, self).__init__()
-        self.surf = pygame.Surface((75, 25))
+        self.surf = pygame.Surface((25, 25))
         self.surf.fill((255, 0, 255))
         self.rect = self.surf.get_rect()
+        self.max_width = screen_width
+        self.max_height = screen_height
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > self.max_width:
+            self.rect.right = self.max_width
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        elif self.rect.bottom >= self.max_height:
+            self.rect.bottom = self.max_height
